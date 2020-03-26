47348fd0847b83412f3565c49c748d56ca566656
Eric Settels <settels.eric@gmail.com>
2020-03-26 14:04:44 -0700

    add further branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
-$(documen).ready(function() {
+$(document).ready(function() {
   $('form#triangle').submit(function(event) {
     event.preventDefault()
     var one = parseInt($('input#one').val())
     var two = parseInt($('input#two').val())
     var three = parseInt($('input#three').val())
+    var result = ''
     if (one === two && two === three) {
-      //equilateral
+      result = 'equilateral'
+      console.log('equilateral')
     } else {
       if ((one === two && two !== three) || (two === three && three !== one)) {
-        //isosceles
+        result = 'isosceles'
       } else {
         if (one !== two && two !== three && one + two > three && one + three > two && two + three > one) {
-          //scalene
+          result = 'scalene'
         } else {
-          //not a triangle
+          result = 'not a triangle'
         }
       }
+    }
+    if (result !== 'not a triangle') {
+      $('#result').html(`<h3>This is ${result}</h3>
+      <h2>Good job</h2>`)
+    } else {
+      alert('not a triangle dummy')
     }
   })
 })
